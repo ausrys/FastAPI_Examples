6003323d4670285ba9f5bb3b1ab16898e73a3fdc
Ausrys Work <ausrys.mikoliunas@orioninc.com>
2025-05-09 16:51:32 +0300

    added routes for workout app

--- FastAPI_sport_collector/celery_app.py
+++ FastAPI_sport_collector/celery_app.py
@@ -2,8 +2,8 @@ from celery import Celery
 
 celery_app = Celery(
     "worker",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0"
+    broker='redis://localhost:6380/1',
+    backend='redis://localhost:6380/1'
 )
 
 celery_app.conf.timezone = "UTC"
